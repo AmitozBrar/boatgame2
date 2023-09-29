1fd04e6392abe29710c6c967be967adff285cb61
AmitozBrar <97923457+AmitozBrar@users.noreply.github.com>
2023-09-29 17:52:41 -0400

    fixed boat.cs

--- Assets/Scripts/boatcontroller2.cs
+++ Assets/Scripts/boatcontroller2.cs
@@ -9,22 +9,18 @@ public class boatcontroller2 : MonoBehaviour
     Rigidbody rb;
     public float playerSpeed = 2.0f;
     //private CharacterController controller;
-    private Vector2 movementInput = Vector2.zero;
-    private Vector2 rotateInput = Vector2.zero;
-     float HorizontalInput;
-    float VerticalInput;
     
     public float sensX;
     public float sensY;
     float xRotation;
     float yRotation;
-    public bool trigger = false;
+
     public GameObject cannonball;
 
     public GameObject particle;
     public Transform Barrel;
     Collider hitbox;
-    //non editable variables
+
     private float lastShot;
     public float cooldown;
     Vector3 moveDirection;
@@ -35,34 +31,17 @@ public class boatcontroller2 : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
+
         x=0;
         y=0;
         hitbox = GetComponent<Collider>();
-        //hitbox.isTrigger = false;
+
         Debug.Log("Trigger On : " + hitbox.isTrigger);
-       // controller = gameObject.GetComponent<CharacterController>();
     }
-
-    // public void OnMove(InputAction.CallbackContext context){
-    //     movementInput = context.ReadValue<Vector2>();
-    // }
-
-    // public void OnShoot(InputAction.CallbackContext context){
-    //     trigger = context.ReadValue<float>() > 0.5f; // ReadValue returns a float for buttons
-    // }
-
-    // public void OnRotation(InputAction.CallbackContext context){
-    //     rotateInput = context.ReadValue<Vector2>();
-    // }
 
     void Update()
     {
         MyInput();
-        //Vector3 move = new Vector3(movementInput.x, 0,movementInput.y);
-        //controller.Move(move * Time.deltaTime * playerSpeed);
-
     }
     private void MyInput(){
          if(Input.GetKey(KeyCode.W)){
@@ -87,8 +66,7 @@ public class boatcontroller2 : MonoBehaviour
 
 
     private void MovePlayer(){
-        //float mouseX = rotateInput.x * Time.deltaTime *sensX;
-        //float mouseY = rotateInput.y * Time.deltaTime *sensY;
+        
         float mouseX = x * Time.deltaTime *sensX;
         float mouseY = y * Time.deltaTime *sensY;
         
@@ -98,8 +76,7 @@ public class boatcontroller2 : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation,0 );
         moveDirection = orientation.forward * y + orientation.right * 0;
-        //moveDirection = orientation.forward * VerticalInput + orientation.right * HorizontalInput;
-        //controller.Move(moveDirection*Time.deltaTime * playerSpeed);
+ 
         rb.AddForce(moveDirection.normalized * playerSpeed * 10f, ForceMode.Force);
     }
     // Update is called once per frame
